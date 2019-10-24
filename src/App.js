109c8aa051ffac4f702b28d90ed6ae752acb6dfb
Glitch (starter-react-template) <none>
2019-10-24 23:11:30 +0000

    🖐💆 Checkpoint ./src/App.js:6341393/7802 ./test.js:6341393/770 ./emojiList.json:6341393/197053

--- src/App.js
+++ src/App.js
@@ -1,31 +1,93 @@
 import React, { PureComponent } from "react";
-import Header from "./Header";
-import SearchInput from "./SearchInput";
-import EmojiResults from "./EmojiResults";
-import filterEmoji from "./filterEmoji";
+import PropTypes from "prop-types";
+import Clipboard from "clipboard";
+import emojiList from "../emojiList.json";
 
-class Header extends PureComponent {
+function filterEmoji(searchText, maxResults) {
+  return emojiList
+    .filter(emoji => {
+      if (emoji.title.toLowerCase().includes(searchText.toLowerCase())) {
+        return true;
+      }
+      if (emoji.keywords.includes(searchText)) {
+        return true;
+      }
+      return false;
+    })
+    .slice(0, maxResults);
+}
+
+
+class EmojiResultsRow extends PureComponent {
+  static propTypes = {
+    title: PropTypes.string,
+    symbol: PropTypes.string
+  };
+
   render() {
+    const codePointHex = this.props.symbol.codePointAt(0).toString(16);
+    const src = `//cdn.jsdelivr.net/emojione/assets/png/${codePointHex}.png`;
     return (
-      <header className="component-header">
-        <img
-          src="//cdn.jsdelivr.net/emojione/assets/png/1f638.png"
-          width="32"
-          height="32"
-          alt=""
-        />
-        Emoji Search
-        <img
-          src="//cdn.jsdelivr.net/emojione/assets/png/1f63a.png"
-          width="32"
-          height="32"
-          alt=""
-        />
-      </header>
+      <div
+        className="component-emoji-result-row copy-to-clipboard"
+        data-clipboard-text={this.props.symbol}
+      >
+        <img alt={this.props.title} src={src} />
+        <span className="title">{this.props.title}</span>
+        <span className="info">Click to copy emoji</span>
+      </div>
     );
   }
 }
 
+class EmojiResults extends PureComponent {
+  static propTypes = {
+    emojiData: PropTypes.array
+  };
+
+  componentDidMount() {
+    this.clipboard = new Clipboard(".copy-to-clipboard");
+  }
+
+  componentWillUnmount() {
+    this.clipboard.destroy();
+  }
+
+  render() {
+    return (
+      <div className="component-emoji-results">
+        {this.props.emojiData.map(emojiData => (
+          <EmojiResultsRow
+            key={emojiData.title}
+            symbol={emojiData.symbol}
+            title={emojiData.title}
+          />
+        ))}
+      </div>
+    );
+  }
+}
+
+
+class SearchInput extends PureComponent {
+  static propTypes = {
+    textChange: PropTypes.func
+  };
+
+  handleChange = event => {
+    this.props.textChange(event);
+  };
+
+  render() {
+    return (
+      <div className="component-search-input">
+        <div>
+          <input onChange={this.handleChange} />
+        </div>
+      </div>
+    );
+  }
+}
 
 class App extends PureComponent {
   constructor(props) {
@@ -35,16 +97,30 @@ class App extends PureComponent {
     };
   }
 
-  handleSearchChange = event => {
+  handleSearchChange(event) {
     this.setState({
       filteredEmoji: filterEmoji(event.target.value, 20)
     });
-  };
+  }
 
   render() {
     return (
       <div>
-        <Header />
+        <header className="component-header">
+          <img
+            src="//cdn.jsdelivr.net/emojione/assets/png/1f638.png"
+            width="32"
+            height="32"
+            alt=""
+          />
+          Emoji Search
+          <img
+            src="//cdn.jsdelivr.net/emojione/assets/png/1f63a.png"
+            width="32"
+            height="32"
+            alt=""
+          />
+        </header>
         <SearchInput textChange={this.handleSearchChange} />
         <EmojiResults emojiData={this.state.filteredEmoji} />
       </div>
@@ -52,4 +128,4 @@ class App extends PureComponent {
   }
 }
 
-export default /App;
+export default App;
